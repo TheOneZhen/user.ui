415f5dbdac55f213cb319994008c50473793584a
TheOneZ <574937561@qq.com>
2021-12-19 23:39:30 +0800

    页面布局开始

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue-demi";
+export default defineComponent({
+    name: 'header',
+    components: {},
+    props: {
+        
+    },
+    beforeMount() {
+
+    },
+    mounted() {
+
+    },
+    data() {
+
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
