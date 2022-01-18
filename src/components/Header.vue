c1d70ea7d1394e55d64188db577744e7ccca8314
LiuZhen <574937561@qq.com>
2022-01-18 20:19:29 +0800

    切换git源

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -14,26 +14,23 @@
         <span></span>
         <el-icon v-html="item.icon"></el-icon>
       </template>
-      <el-menu-item-group
-        v-for=""
+      <el-menu-item-graoup
+        v-for="(child, index) in item.children"
+        :key="index"
       >
         <template #title></template>
         <el-menu-item 
           index=""
+
         ></el-menu-item>
-      </el-menu-item-group>
+      </el-menu-item-graoup>
     </el-sub-menu>
   </el-menu>
 </template>
 
 <script lang="ts">
-<<<<<<< HEAD
-import { elMenu}
-import { defineComponent } from 'vue-demi'
-=======
 import { Header } from '@/DS/menu';
 import { defineComponent } from 'vue'
->>>>>>> 1125fff840af8f12006dfc8b5b3295284bff2e09
 
 export default defineComponent({
   name: 'header',
@@ -85,40 +82,6 @@ export default defineComponent({
       menuCollapse () {
         return false;
       }
-<<<<<<< HEAD
-  },
-  data () {
-    return {
-      navigationList: [
-        {
-          title: "第一页",
-          icon: "",
-          disabled: false,
-          children: []
-        },
-        {
-          title: "编程记",
-          icon: "",
-          disabled: false,
-          children: []
-        },
-        {
-          title: "藏宝库",
-          icon: "",
-          disabled: false,
-          children: []
-        },
-        {
-          title: "call me",
-          icon: "",
-          disabled: false,
-          children: []
-        }
-      ]
-
-    }
-=======
->>>>>>> 1125fff840af8f12006dfc8b5b3295284bff2e09
   }
 })
 </script>
