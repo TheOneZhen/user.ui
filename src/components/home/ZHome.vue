87231a0f8e3afedf6c91bb2357395ee6d0765223
TheOneZ <574937561@qq.com>
2022-04-13 23:10:23 +0800

    新增目录

--- src/components/home/ZHome.vue
+++ src/components/home/ZHome.vue
@@ -9,8 +9,7 @@
           </div>
         </el-col>
       </el-row>
-    </el-col>
-  </el-row>
+    </el-col></el-row>
 </template>
 
 <script lang='ts' setup>
@@ -25,77 +24,5 @@ function getDoodle (row: number, col: number) {
 </script>
 
 <style lang='scss' scoped>
-css-doodle {
-  --gridSize: 8em;
-  --bk: #60569E;
-  --RandRect: (
-    @grid: 5 / var(--gridSize);
-    background: var(--bk);
-    transform: scale(@rand(0.2, 0.9));
-  );
-  --TransformRect: (
-    :doodle {
-      --s: 0turn;
-      grid-gap: 1px;
-    }
-    :doodle(:active) {
-      --s: 0.5turn;
-    }
-    @grid: 3 / var(--gridSize);
-    transition: 1s cubic-bezier(.175, .885, .32, 1.275);
-    background: var(--bk);
-    transform: rotate(@var(--s));
-  );
-  --AbstractShape: (
-    @grid: 5 / var(--gridSize);
-    background: linear-gradient(
-      @rand(360deg),
-      @stripe(#60569E, #E6437D, #EBBF4D)
-    );
-  );
-  --AbstractCircle: (
-    @grid: 1 / var(--gridSize);
-    background: radial-gradient(
-      circle at @r(100%) @r(100%),
-      @m20(
-        @p(#60569e, #ebbf4d) calc(@n(-1) * 100% / @N),
-        @lp calc(@n * 100% / @N)
-      )
-    );
-  );
-  --RandTriangle: (
-    @grid: 5 / var(--gridSize);
-    background: rgba(96, 86, 158, @rand(.9));
-    transition: .2s ease @rand(200ms);
-    transform: rotate(@rand(360deg));
-    clip-path: polygon(
-      @rand(100%) 0, 100% @rand(100%), 0 @rand(100%)
-    );
-  );
-  --AbstractRiver: (
-    background: @shaders(
-      texture_0 {
-        @grid: 1 / 100%;
-        background: linear-gradient(45deg, @stripe.@m20.@p(
-          #FFF4E0, #F8B501, #06ACB5, #17191D, #FC3D3C
-        ));
-      }
-      fragment {
-        void main() {
-          vec2 coords = vec2(r * cos(angle), r * sin(angle)) + c;
-          FragColor = texture(texture_0, coords);
-        }
-      }
-    );
-  );
-  --Z: (
-    background: @svg(
-      viewBox: 0 0 1 1;
-      path {
-        d: M 0 0 L 1 0 L 1 1;
-        fill: red;
-      }
-    );
-  );
-}
+@import "./css/nineGrid.scss"
 </style>
