c665713e91525ab25700d746d61f207912b12501
Z <zhenisbusy@qq.com>
2022-05-07 01:34:45 +0800

    todo

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import 'element-plus/dist/index.css'
 import router from './router'
 import '@/global/scss/index.scss'
 import 'css-doodle'
+import microApp from '@micro-zoe/micro-app'
 
-const app = createApp(App)
-app.use(ElementPlus)
-app.use(router)
-app.mount('#app')
+microApp.start()
+
+createApp(App).use(ElementPlus).use(router).mount('#app')
