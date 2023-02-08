155aa62c6905af34efafac55b8de9ded3529ccf3
liuzhen <liuzhen@mbse.com.cn>
2023-02-08 18:42:54 +0800

    开发规范更新

--- src/components/header/config.ts
+++ src/components/header/config.ts
@@ -18,17 +18,17 @@ export const NavigationMenu: Header.SubMenu[] = [
         icon: 'Cherry.svg'
       },
       {
-        title: '文档翻译',
+        title: '资源文档',
         icon: 'DocumentCopy.svg'
       },
       {
-        title: '我的源码',
+        title: '源码',
         icon: 'Crop.svg'
       }
     ]
   },
   {
-    title: '阶段总结',
+    title: '学习记录',
     icon: 'Document.svg',
     children: [
       {
@@ -38,18 +38,16 @@ export const NavigationMenu: Header.SubMenu[] = [
       {
         title: '源码分析',
         icon: 'FullScreen.svg'
+      },
+      {
+        title: '原理及业务实现',
+        icon: 'FullScreen.svg'
       }
     ]
   },
   {
     title: '留言',
-    icon: 'ChatSquare.svg',
-    children: [
-      {
-        title: '留言区',
-        icon: 'ChatDotSquare.svg'
-      }
-    ]
+    icon: 'ChatSquare.svg'
   },
   {
     title: '发现另外的我',
