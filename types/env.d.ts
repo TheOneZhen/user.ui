f26097e00da1fc560bf7fb861f8231abfad86610
liuzhen <liuzhen@mbse.com.cn>
2023-03-04 17:44:11 +0800

    加上mockjs注解

--- types/env.d.ts
+++ types/env.d.ts
@@ -10,8 +10,3 @@ declare module '*.vue' {
  */
 declare module '@kangc/v-md-editor/lib/theme/github.js'
 declare module '@kangc/v-md-editor/lib/preview'
-declare module 'mockjs' {
-  function mock (rurl: RegExp, any): void
-  function mock (any): void
-  const Random: any
-}
