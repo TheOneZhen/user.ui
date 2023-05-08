ef4ba5a4c4b88a4a9fc136bd1092aae4931d0770
liuzhen <liuzhen@mbse.com.cn>
2023-05-08 16:51:10 +0800

    网站标志

--- src/components/animation/EntryAnimation.vue
+++ src/components/animation/EntryAnimation.vue
@@ -3,6 +3,9 @@
     <title>zhen</title>
     <g stroke="red" >
       <path d="m0,0 h20 l-20,20 l20,20"></path>
+      <path d="m40,0 v40 v-20 h20 v20"></path>
+      <path d="m80,30 h20 v-10 h-20 v20 h20"></path>
+      <path d="m120,20 v20 v-20 h20 v20"></path>
     </g>
   </svg>
 </template>
