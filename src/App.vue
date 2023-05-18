805cb4ea0ce6d55f47f5b57af2ce67b05c3121af
liuzhen <liuzhen@mbse.com.cn>
2023-05-18 12:05:59 +0800

    留言界面预览初步完成

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,12 @@
   <teleport to="body">
     <el-drawer v-model="useDrawerStore.visible"
                append-to-body
+               destroy-on-close
                :before-close="done => done()"
                :direction="useDrawerStore.direction"
-               :title="useDrawerStore.title">zheshiyidaun ceshineirong </el-drawer>
+               :title="useDrawerStore.title">
+               <component :is="useDrawerStore.content"></component>
+    </el-drawer>
   </teleport>
 </template>
 
@@ -13,11 +16,5 @@
 import { UseDrawerStore } from './store/UseDrawerStore'
 
 const useDrawerStore = UseDrawerStore()
-</script>
 
-<style lang="scss">
-html, body {
-  margin: 0;
-  padding: 0;
-}
-</style>
+</script>
--- src/components/lefting/ZLefting.vue
+++ src/components/lefting/ZLefting.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="z-lefting" v-click-out-side="handleChangeDisplay(false)">
+  <div class="z-lefting">
     <el-row>
       <el-col :span="avatarPre">
         <el-avatar src="https://empty" @error="errorHandler">
@@ -12,12 +12,8 @@
                   :autosize="{ minRows: 2, maxRows: 20 }"
                   type="textarea"
                   placeholder="这是一首简单的小情歌"></el-input>
-        <z-markdown-preview v-if="isDisplayContentByMarkdown"
-                            :text="content"
-                            :style="getCommentStyle()"
-                            class="_comment-markdown"></z-markdown-preview>
         <el-row>
-          <el-button @click="handleChangeDisplay(true)">Preview</el-button>
+          <el-button @click="handleDisplayMarkdown()">Preview</el-button>
           <el-button>Commit</el-button>
         </el-row>
       </el-col>
@@ -27,35 +23,24 @@
 </template>
 
 <script lang="ts" setup>
-import { render } from 'vue'
-import { UseDrawerStore } from '../../store/UseDrawerStore'
+import ZMarkdownPreview from '../template/ZMarkdownPreview.vue'
 
 const avatarPre = 1
-
 const content = ref('')
 const comment = ref<any>(null)
-const isDisplayContentByMarkdown = ref(false)
-const useDrawerStoew = UseDrawerStore()
+const useDrawerStore = app.store.get('UseDrawerStore')
 
 function errorHandler () {
   console.error("User image loads failed!")
 }
 
-function handleChangeDisplay (value: boolean) {
-  isDisplayContentByMarkdown.value = value
-  useDrawerStoew.on()
+function handleDisplayMarkdown () {
+  useDrawerStore.on(
+    '预览',
+    'ltr',
+    h(ZMarkdownPreview, { text: content.value })
+  )
 }
-
-function getCommentStyle () {
-  const result = { width: '0px', height: '0px' }
-  if (comment.value?.ref) {
-    const commentDom = comment.value.ref;
-    result.width = commentDom.width;
-    result.height = commentDom.height;
-  }
-  return result;
-}
-
 </script>
 
 <style lang="scss" scoped>
