b4c4fb63f56b150ab8fe33fadd9e6a800345db32
Z <zhenisbusy@qq.com>
2023-07-22 19:11:32 +0800

    界面更新

--- src/components/header/ZHeader.vue
+++ src/components/header/ZHeader.vue
@@ -6,22 +6,20 @@
     <el-menu-item v-for="menuItem in NavigationMenu"
                   :key='menuItem.title'
                   :index="menuItem.title"
-                  @click="handleClick(menuItem.title)">
-      <el-icon><span :[menuItem.icon]="''"></span></el-icon>
+                  @click="() => app.router?.push({ name: menuItem.title })">
+      <el-icon :[menuItem.icon]="''" />
     </el-menu-item>
     <div class="flex-grow-1"></div>
-    <el-menu-item index="github" @click="handleGitHub">
-      <el-icon>
-        <span class="_color-scheme" icon-carbon:logo-github></span>
-      </el-icon>
+    <el-menu-item index="github">
+      <el-link href="https://github.com/TheOneZhen" target="_blank" :underline="false">
+        <el-icon icon-carbon:logo-github />
+      </el-link>
     </el-menu-item>
     <el-menu-item index="color-scheme"
                   @mousedown="handleMousedown"
                   @mouseleave="handleMouseleave"
                   @mouseup="handleMouseup">
-      <el-icon>
-        <span class="_color-scheme" icon-carbon:awake></span>
-      </el-icon>
+      <el-icon class="_color-scheme" icon-carbon:awake />
     </el-menu-item>
   </el-menu>
 </template>
@@ -49,13 +47,6 @@ function handleMouseup () {
   handleMouseleave()
 }
 
-function handleClick (name: typeof NavigationMenu[0]['title']) {
-  app.router?.push({ name })
-}
-
-function handleGitHub () {
-  window.open('https://github.com/TheOneZhen')
-}
 </script>
 
 <style lang="scss" scoped>
