94b516cd6f80fd36f876fec5622556fb3732657d
Z <zhenisbusy@qq.com>
2023-10-22 22:58:04 +0800

    不知道修改了什么鬼东西

--- src/main.ts
+++ src/main.ts
@@ -10,21 +10,21 @@ import { ClickOutside, vLoading } from 'element-plus'
 import { vActive } from './utils/active'
 
 microApp.start({
-  plugins: {
-    modules: {
-      userEarthMap: [{
-        loader (code) {
-          if (process.env.NODE_ENV === 'development') {
-            // 这里 basename 需要和子应用vite.config.js中base的配置保持一致
-            code = code.replace(/(from|import)(\s*['"])(\/userEarthMap\/)/g, all => {
-              return all.replace('/userEarthMap/', 'http://localhost:3050/userEarthMap/')
-            })
-          }
-          return code
-        }
-      }]
-    }
-  }
+  // plugins: {
+  //   modules: {
+  //     userEarthMap: [{
+  //       loader (code) {
+  //         if (process.env.NODE_ENV === 'development') {
+  //           // 这里 basename 需要和子应用vite.config.js中base的配置保持一致
+  //           code = code.replace(/(from|import)(\s*['"])(\/userEarthMap\/)/g, all => {
+  //             return all.replace('/userEarthMap/', 'http://localhost:3050/userEarthMap/')
+  //           })
+  //         }
+  //         return code
+  //       }
+  //     }]
+  //   }
+  // }
 })
 
 const AppInstance = createApp(App)
