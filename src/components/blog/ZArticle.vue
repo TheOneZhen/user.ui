c269e8452a2fc1e25e9ae24b7db65fbc5f6ffd2c
TheOneZ <574937561@qq.com>
2024-02-16 03:22:03 +0800

    移除article中骨架屏

--- src/components/blog/ZArticle.vue
+++ src/components/blog/ZArticle.vue
@@ -1,38 +1,29 @@
 <template>
-  <div class="z-article g-padding-5%-4% g-background-#FFFFFF">
-    <el-skeleton :throttle="100">
-      <template #template v-if="article">
-        <h1 class="z-article-title g-text-align-center g-margin-0-0-5%-0" v-html="article.title"></h1>
-        <p class="g-opacity-.4 g-text-align-right">
-          {{ app.blog.formatDate(article.createTime) }}
-        </p>
-        <div class="z-article-tag g-margin-0-0-5%-0">
-          <el-tag v-for="tag in tags"
-                  :index="tag.id"
-                  v-text="tag.title"/>
-        </div>
-        <component :is="customMarked.parse(article.content, true)" />
-        <p class="g-opacity-.4 g-text-align-right">
-          {{ '最近修改：' + app.blog.formatDate(article.updateTime) }}
-        </p>
-        <el-button circle class="g-position-fixed g-left-92% g-bottom-280px">
-          <el-icon icon-ic:round-remove-red-eye />
-          {{ article.views }}
-        </el-button>
-        <el-button circle v-active="viewRecord['Like Article'].has(article.id)" class="g-position-fixed g-left-92% g-bottom-220px g-margin-left-0" @click="app.user.lnArticle(article.id, 1)">
-          <el-icon icon-material-symbols:favorite />
-        </el-button>
-        <el-button circle v-active="viewRecord['Dislike Article'].has(article.id)" class="g-position-fixed g-left-92% g-bottom-160px" @click="app.user.lnArticle(article.id, 0)">
-          <el-icon icon-material-symbols:heart-broken />
-        </el-button>
-      </template>
-      <template #default>
-        <el-skeleton :rows="8" animated />
-      </template>
-    </el-skeleton>
-    <el-button circle
-               class="g-position-fixed g-left-92% g-bottom-100px"
-               @click="hanldeComment">
+  <div v-if="article" class="z-article g-padding-5%-4% g-background-#FFFFFF">
+    <h1 class="z-article-title g-text-align-center g-margin-0-0-5%-0" v-html="article.title"></h1>
+    <p class="g-opacity-.4 g-text-align-right">
+      {{ app.blog.formatDate(article.createTime) }}
+    </p>
+    <div class="z-article-tag g-margin-0-0-5%-0">
+      <el-tag v-for="tag in tags" :index="tag.id" v-text="tag.title" />
+    </div>
+    <component :is="customMarked.parse(article.content, true)" />
+    <p class="g-opacity-.4 g-text-align-right">
+      {{ '最近修改：' + app.blog.formatDate(article.updateTime) }}
+    </p>
+    <el-button circle class="g-position-fixed g-left-92% g-bottom-280px">
+      <el-icon icon-ic:round-remove-red-eye />
+      {{ article.views }}
+    </el-button>
+    <el-button circle v-active="viewRecord['Like Article'].has(article.id)"
+      class="g-position-fixed g-left-92% g-bottom-220px g-margin-left-0" @click="app.user.lnArticle(article.id, 1)">
+      <el-icon icon-material-symbols:favorite />
+    </el-button>
+    <el-button circle v-active="viewRecord['Dislike Article'].has(article.id)"
+      class="g-position-fixed g-left-92% g-bottom-160px" @click="app.user.lnArticle(article.id, 0)">
+      <el-icon icon-material-symbols:heart-broken />
+    </el-button>
+    <el-button circle class="g-position-fixed g-left-92% g-bottom-100px" @click="hanldeComment">
       <el-icon icon-pajamas:comment-dots />
     </el-button>
     <!-- 上一篇 -->
@@ -62,7 +53,7 @@ const tags = computed(() => {
   return result
 })
 
-watch (() => index, (id) => getArticle(+id))
+watch(() => index, (id) => getArticle(+id))
 
 function getArticle (index: ArticleType['id']) {
   article.value = null
@@ -79,7 +70,7 @@ getArticle(+index)
 
 <style lang="scss">
 .z-article {
-  .el-button + .el-button {
+  .el-button+.el-button {
     margin-left: 0;
   }
 }
