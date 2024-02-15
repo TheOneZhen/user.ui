9fe17c7cd5bf62e02b870ab01eed840849097dc3
TheOneZ <574937561@qq.com>
2024-02-16 03:25:48 +0800

    布局

--- src/components/layout/ZLayout.vue
+++ src/components/layout/ZLayout.vue
@@ -1,17 +1,22 @@
 <template>
-  <el-scrollbar id="z-main-scroll">
+  <el-scrollbar wrap-class="z-main-scroll">
     <css-doodle ref="doodle" class="g-position-absolute" click-to-update use="var(--rule)" />
-    <div class="g-relative g-min-height-100dvh" @click.self="() => doodle.update()" >
+    <div class="g-relative g-min-height-100dvh" @click.self="() => doodle.update()">
       <z-header />
       <div class="g-flex">
-        <div style="display: var(--theme-layout-left-display); flex-basis: var(--theme-layout-left);"></div>
+        <div id="z-layout-sider-left" style="display: var(--theme-layout-left-display); flex-basis: var(--theme-layout-left);"></div>
         <router-view class="g-relative g-flex-auto" />
-        <div style="display: var(--theme-layout-right-display); flex-basis: var(--theme-layout-right);"></div>
+        <div id="z-layout-sider-right" style="display: var(--theme-layout-right-display); flex-basis: var(--theme-layout-right);"></div>
       </div>
       <!-- <z-bullet-screen></z-bullet-screen> -->
     </div>
-    <z-signature v-if="tasks.length > 0" :loading="true" width="100" class="g-position-fixed g-left-1% g-bottom-10px"></z-signature>
-    <el-backtop class="g-left-92%" ></el-backtop>
+    <!-- to right -->
+    <z-signature v-if="tasks.length > 0" loading width="100"
+      class="g-position-fixed g-left-1% g-bottom-10px"></z-signature>
+    <!-- to left -->
+    <Teleport to="body">
+      <el-backtop target=".z-main-scroll" class="g-left-92%"></el-backtop>
+    </Teleport>
     <z-footer />
   </el-scrollbar>
 </template>
@@ -32,11 +37,14 @@ css-doodle {
       background: @var(--theme-background);
       transition: background 1s ease;
     }
+
     transition: @r(0.3, 0.7)s ease all;
+
     @nth(1) {
       position: absolute;
       @place-cell: @m2(@r(0, 100)%);
       @size: @r(3vmin, 20vmin) @lr;
+
       ::before, ::after {
         content: "";
         position: absolute;
@@ -46,28 +54,32 @@ css-doodle {
         border-radius: 100%;
         transition: inherit;
       }
+
       ::before {
         opacity: 0.25;
         transform: scale(@r(1.01, 1.5));
       }
+
       ::after {
         border: 1px solid #000;
       }
     }
+
     @nth(2, 3, 4, 5, 6, 7) {
       position: absolute;
       left: @r(0, 100)%;
       bottom: 0;
-      @size:@r(8vmin, 20vmin) @lr;
+      @size: @r(8vmin, 20vmin) @lr;
       background: @var(--theme-bar);
       border: 1px solid #000;
       transform: translateY(@p(50%, 100%)) rotate(45deg) skew(@r(-30deg, 30deg), @lr);
     }
+
     @nth(8, 9, 10, 11, 12, 13, 14, 15, 16, 17) {
       position: absolute;
       left: @r(0, 100)%;
       bottom: 0;
-      @size:@r(2vmin, 8vmin) @lr;
+      @size: @r(2vmin, 8vmin) @lr;
       background: @var(--theme-compete);
       border: 1px solid #000;
       transform: translateY(@p(50%, 100%)) rotate(45deg) skew(@r(30, 40deg), @lr);
